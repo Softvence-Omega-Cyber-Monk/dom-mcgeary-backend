fb03a0290454c1198bb0f0087e704061a73445c6
Mohin-Uddin-Mishrat <mmishrat90@gmail.com>
2025-11-13 14:16:48 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ["http://localhost:5173","http://localhost:5174","https://dom-mcgeary-frontend.vercel.app/"],
+    origin: ["http://localhost:5173","http://localhost:5174","https://dom-mcgeary-frontend.vercel.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
